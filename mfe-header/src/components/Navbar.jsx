b03f7e7ad1d8b9b980c15d9abd1a93b964e8a7ee
Davidc1409 <davidc2509@gmail.com>
2026-03-26 13:29:52 +0100

    feat: shared event bus between mfe

--- mfe-header/src/components/Navbar.jsx
+++ mfe-header/src/components/Navbar.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import eventBus from "shared/eventBus";
+import "./Navbar.css";
 
-function Navbar({ notifications = 0 }) {
+function Navbar() {
+  const [notifications, setNotifications] = useState(0);
+
+  useEffect(() => {
+    // TODO: quand un joueur rejoint une partie, incrementer le badge notifications
+    // Penser au cleanup React
+    const unsubscribe = eventBus.on("game:joined", (data) => {
+      setNotifications((prev) => prev + 1);
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
--- mfe-header/webpack.config.js
+++ mfe-header/webpack.config.js
@@ -1,13 +1,13 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { ModuleFederationPlugin } = require("webpack").container;
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
-    publicPath: "http://localhost:3001/",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: 'http://localhost:3001/',
     clean: true,
   },
   devServer: {
@@ -15,7 +15,7 @@ module.exports = {
     hot: true,
     historyApiFallback: true,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      'Access-Control-Allow-Origin': '*',
     },
   },
   module: {
@@ -24,32 +24,38 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
+    alias: {
+      shared: path.resolve(__dirname, '../shared'),
+    },
   },
   plugins: [
     new ModuleFederationPlugin({
-      // TODO: configurer ce MFE pour qu'il expose le composant Navbar
-      // Documentation: https://webpack.js.org/plugins/module-federation-plugin/
-      name: "mfeHeader",
-      filename: "remoteEntry.js",
-      exposes: { "./Navbar": "./src/components/Navbar" },
-      shared: { react: { singleton: true }, "react-dom": { singleton: true } },
+      name: 'mfeHeader',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Navbar': './src/components/Navbar',
+      },
+      shared: {
+        react: { singleton: true, requiredVersion: '^18.2.0' },
+        'react-dom': { singleton: true, requiredVersion: '^18.2.0' },
+      },
     }),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
 };
