41e9ce2a7fd5986e32ac810139efc791fc1c3456
Davidc1409 <davidc2509@gmail.com>
2026-03-26 14:43:25 +0100

    feat: add catalog

--- mfe-header/src/components/Navbar.jsx
+++ mfe-header/src/components/Navbar.jsx
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from "react";
-import eventBus from "shared/eventBus";
-import "./Navbar.css";
+import React, { useState, useEffect } from 'react';
+import eventBus from 'shared/eventBus';
+import './Navbar.css';
 
 function Navbar() {
   const [notifications, setNotifications] = useState(0);
 
   useEffect(() => {
-    // TODO: quand un joueur rejoint une partie, incrementer le badge notifications
-    // Penser au cleanup React
-    const unsubscribe = eventBus.on("game:joined", (data) => {
-      setNotifications((prev) => prev + 1);
+    const unsub = eventBus.on('game:joined', () => {
+      setNotifications(prev => prev + 1);
     });
-    return unsubscribe;
+    return () => unsub();
   }, []);
 
   return (
